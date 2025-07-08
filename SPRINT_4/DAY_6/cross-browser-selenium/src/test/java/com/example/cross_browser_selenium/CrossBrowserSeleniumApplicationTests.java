e340007f982ba1ad4098688deb8ab75ef2188a21
Sankhadip Roy <22051787@kiit.ac.in>
2025-07-08 16:57:35 +0530

    DAY_6

---
+++ SPRINT_4/DAY_6/cross-browser-selenium/src/test/java/com/example/cross_browser_selenium/CrossBrowserSeleniumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cross_browser_selenium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrossBrowserSeleniumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
